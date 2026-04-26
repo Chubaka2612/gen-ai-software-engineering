38edac42d6f7fc7de22bc6530e036f1fd24c03e9
Viktoriia Skirko <Viktoriia_Skirko@epam.com>
2026-04-26 19:28:00 +0300

    chore: add swagger

--- homework-1/src/AiCraft.Banking/Program.cs
+++ homework-1/src/AiCraft.Banking/Program.cs
@@ -1,17 +1,25 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "AiCraft.Banking API", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
